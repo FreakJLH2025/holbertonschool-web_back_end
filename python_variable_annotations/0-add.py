27a5f5a8184c3af33668107dcabfb1a9315ecd7a
FreakJLH2025 <joshua.luja@gmail.com>
2026-04-09 19:15:23 +0000

    0-add.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+Defines a type-annotated function add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of a and b as a float.
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float: sum of a and b
+    """
+    return a + b
