6fa3d1568007dce63c7cd07b6c85bf21b95353cb
FreakJLH2025 <joshua.luja@gmail.com>
2026-04-30 17:20:35 +0000

    10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,32 +1,20 @@
 // 10-car.js
 
-const _cloneSymbol = Symbol('cloneCar');
-
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('Brand must be a string');
-    }
-    if (typeof motor !== 'string') {
-      throw new TypeError('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw new TypeError('Color must be a string');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  // Método cloneCar usando Symbol
-  [_cloneSymbol]() {
-    const Cls = this.constructor; // referencia dinámica a la clase actual
-    return new Cls(this._brand, this._motor, this._color);
+  // Definir species para que subclases puedan controlar qué clase se usa al clonar
+  static get [Symbol.species]() {
+    return this;
   }
 
-  // Exponer cloneCar como método público
+  // Método cloneCar que usa Symbol.species
   cloneCar() {
-    return this[_cloneSymbol]();
+    const Cls = this.constructor[Symbol.species];
+    return new Cls();
   }
 }
